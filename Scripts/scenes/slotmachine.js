e49a68471858b7d3f495bdd3467971708c02626f
VivianSs <vivianss0301@gmail.com>
2016-02-23 02:38:11 -0500

    updated reels function

--- Scripts/scenes/slotmachine.js
+++ Scripts/scenes/slotmachine.js
@@ -3,7 +3,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-// SLOT_MACHINE SCENE
+// MENU SCENE
 var scenes;
 (function (scenes) {
     var SlotMachine = (function (_super) {
@@ -26,37 +26,32 @@ var scenes;
             // add background image to the scene
             this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
             this.addChild(this._backgroundImage);
-            //add Bet1Button to the scene
+            // add Bet1Button to the scene
             this._bet1Button = new objects.Button("Bet1Button", 168, 382, false);
             this.addChild(this._bet1Button);
-            this._bet1Button.on("click", this._bet1ButtonClick);
-            //add Bet10Button to the scene
+            this._bet1Button.on("click", this._bet1ButtonClick, this);
+            // add Bet10Button to the scene
             this._bet10Button = new objects.Button("Bet10Button", 240, 382, false);
             this.addChild(this._bet10Button);
-            this._bet10Button.on("click", this._bet10ButtonClick);
-            //add Bet100Button to the scene
+            this._bet10Button.on("click", this._bet10ButtonClick, this);
+            // add Bet100Button to the scene
             this._bet100Button = new objects.Button("Bet100Button", 312, 382, false);
             this.addChild(this._bet100Button);
-            this._bet100Button.on("click", this._bet100ButtonClick);
-            //add SpinButton to the scene
+            this._bet100Button.on("click", this._bet100ButtonClick, this);
+            // add SpinButton to the scene
             this._spinButton = new objects.Button("SpinButton", 402, 382, false);
             this.addChild(this._spinButton);
-            this._spinButton.on("click", this._spinButtonClick);
+            this._spinButton.on("click", this._spinButtonClick, this);
             // add this scene to the global stage container
             stage.addChild(this);
         };
         // SLOT_MACHINE Scene updates here
         SlotMachine.prototype.update = function () {
         };
-        //private methods
+        //PRIVATE METHODS
         /* Utility function to check if a value falls within a range of bounds */
         SlotMachine.prototype._checkRange = function (value, lowerBounds, upperBounds) {
-            if (value >= lowerBounds && value <= upperBounds) {
-                return value;
-            }
-            else {
-                return -1;
-            }
+            return (value >= lowerBounds && value <= upperBounds) ? value : -1;
         };
         /* When this function is called it determines the betLine results.
         e.g. Bar - Orange - Banana */
--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -1,7 +1,7 @@
-// SLOT_MACHINE SCENE
+// MENU SCENE
 module scenes {
     export class SlotMachine extends objects.Scene {
-        //PRIVATE INSTANCE VARIABLES ++++++++++++   
+        //PRIVATE INSTANCE VARIABLES ++++++++++++
         private _backgroundImage: createjs.Bitmap;
         private _bet1Button: objects.Button;
         private _bet10Button: objects.Button;
@@ -16,7 +16,6 @@ module scenes {
         private _bells = 0;
         private _sevens = 0;
         private _blanks = 0;
-        
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
             super();
@@ -25,32 +24,33 @@ module scenes {
         // PUBLIC METHODS +++++++++++++++++++++
         
         // Start Method
-        public start(): void {
+        public start(): void {    
             // add background image to the scene
             this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
             this.addChild(this._backgroundImage);
             
-            //add Bet1Button to the scene
+            // add Bet1Button to the scene
             this._bet1Button = new objects.Button("Bet1Button", 168, 382, false);
             this.addChild(this._bet1Button);
-            this._bet1Button.on("click", this._bet1ButtonClick);
+            this._bet1Button.on("click", this._bet1ButtonClick, this); 
             
-            //add Bet10Button to the scene
+            // add Bet10Button to the scene
             this._bet10Button = new objects.Button("Bet10Button", 240, 382, false);
             this.addChild(this._bet10Button);
-            this._bet10Button.on("click", this._bet10ButtonClick);
+            this._bet10Button.on("click", this._bet10ButtonClick, this); 
             
-            //add Bet100Button to the scene
+            // add Bet100Button to the scene
             this._bet100Button = new objects.Button("Bet100Button", 312, 382, false);
             this.addChild(this._bet100Button);
-            this._bet100Button.on("click", this._bet100ButtonClick);
+            this._bet100Button.on("click", this._bet100ButtonClick, this); 
             
-            //add SpinButton to the scene
+            // add SpinButton to the scene
             this._spinButton = new objects.Button("SpinButton", 402, 382, false);
             this.addChild(this._spinButton);
-            this._spinButton.on("click", this._spinButtonClick);
-            
-            
+            this._spinButton.on("click", this._spinButtonClick, this); 
+        
+         
+        
             // add this scene to the global stage container
             stage.addChild(this);
         }
@@ -60,15 +60,10 @@ module scenes {
 
         }
         
-        //private methods
+        //PRIVATE METHODS
         /* Utility function to check if a value falls within a range of bounds */
         private _checkRange(value: number, lowerBounds: number, upperBounds: number): number {
-            if (value >= lowerBounds && value <= upperBounds) {
-                return value;
-            }
-            else {
-                return -1;
-            }
+            return (value >= lowerBounds && value <= upperBounds) ? value : -1;
         }
         
         /* When this function is called it determines the betLine results.
